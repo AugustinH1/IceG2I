2ad0fd80fc79dd9933f30dab401afb7e972f16ed
AugustinH1 <98890365+AugustinH1@users.noreply.github.com>
2022-05-22 14:55:45 +0200

    Delete IceG2I directory

--- IceG2I/libs/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$BDD_host="localhost";
-$BDD_user="lara";
-$BDD_password="Laracaro59112?";
-$BDD_base="IceG2I";
-
-?>
--- IceG2I/templates/detail_produit.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$id=$_GET["id_produit"];
-    echo "produit $id";
-?>
